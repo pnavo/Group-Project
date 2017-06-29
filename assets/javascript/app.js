7ba9fc70eaf347e342b6576873468bf4afdd90a2
Phillip Navo <shrimp87@calvisitor-10-105-144-191.calvisitor.1918.berkeley.edu>
2017-06-29 09:44:44 -0700

    Inital files added with .js notes

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// Initialize Firebase??
+
+// Set global variables (i.e. email, phone number, etc)
+// Set up arrays with pics/quotes
+
+// Set on-click function for submit button
+	// Pull data from fields to generate random image to send to user
+	// Clear all data from entry fields
+
+//
+//
+//
+//
